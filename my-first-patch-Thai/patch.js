f654f4b82d0cd680fbeb2ccd5fef3cd722cb6331
thcrap-thai-repo-tutorial <78195384+thcrap-thai-repo-tutorial@users.noreply.github.com>
2021-01-29 21:52:15 +0700

    Create patch.js

---
+++ my-first-patch-Thai/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"thpatch/lang_th"
+	],
+	"id": "my-first-patch-of-thai",
+	"servers": [
+		"https://raw.githubusercontent.com/thcrap-thai-repo-tutorial/potential-disco-spicy/master/"
+	],
+	"title": "my-first-patch-Thai"
+}
