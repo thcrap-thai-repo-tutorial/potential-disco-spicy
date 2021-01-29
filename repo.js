bdb05bf60ea4e499abc57131e861624e0a9b5d59
PartyParrotTH <oowo52513@gmail.com>
2021-01-29 14:40:58 +0700

    Repo.js added

--- repo.js
+++ repo.js
@@ -1,5 +1,9 @@
 {
 	"contact": "thaitutorial@thpatch.net",
 	"id": "potential-disco-spicy",
+	"patches": {},
+	"servers": [
+		"https://raw.githubusercontent.com/thai-thcrap-repo-tutorial/potential-disco-spicy/master/"
+	],
 	"title": "Thai Tutorial repository"
 }
